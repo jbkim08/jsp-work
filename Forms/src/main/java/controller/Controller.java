ca156179a636a6367eea66c730a3a43055c5c8a8
jbkim <drv98@naver.com>
2022-10-06 09:38:21 +0900

    Forms

---
+++ Forms/src/main/java/controller/Controller.java
@@ -0,0 +1,29 @@
+package controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/Controller")
+public class Controller extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String user = request.getParameter("user");
+		
+		PrintWriter out = response.getWriter();
+		out.println("<html>");
+		out.println("user : "+ user);
+		out.println("</html>");
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
---
+++ Forms/src/main/webapp/form1.jsp
@@ -0,0 +1,17 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+
+<form action="/Forms/Controller" method="post">
+<input type="text" name="user">
+<input type="submit" value="전송">
+</form>
+
+</body>
+</html>
