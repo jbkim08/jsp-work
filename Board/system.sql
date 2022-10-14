49d58aaa2d65efc8f4eaafb84ba64e2c601827dd
jbkim <drv98@naver.com>
2022-10-14 09:26:22 +0900

    유저 생성및 권한

--- Board/system.sql
+++ Board/system.sql
@@ -3,3 +3,8 @@ EXEC dbms_xdb.sethttpport(9090);
 commit;
 -- 확인하기
 SELECT dbms_xdb.gethttpport() FROM dual;
+
+-- 유저 만들기
+CREATE USER oracle IDENTIFIED BY 1234;
+-- 권한을 줘야 작업 가능(접속 및 테이블생성등)
+GRANT CONNECT, RESOURCE TO oracle;
